7fb4b843cdd7bbec03e5b782bc26a20a410f94f5
Benjamin Pratt <104381242+prattbj@users.noreply.github.com>
2022-12-05 12:56:34 -0700

    Added comments, cleaned up code

--- Game/Scripting/DrawScreen.cs
+++ Game/Scripting/DrawScreen.cs
@@ -8,10 +8,6 @@ namespace Trebuchet.Game.Scripting
 {
     public class DrawScreen
     {
-
-        //double answer = 0;
-
-
         int trebuchetFrame = 0;
         int explosionFrame = 0;
         int framesCounter = 0;
@@ -29,6 +25,7 @@ namespace Trebuchet.Game.Scripting
         private Dictionary<string, Texture2D> textures = new Dictionary<string, Texture2D>();
         public DrawScreen()
         {
+            //Load the images into the dictionary of textures
             Image idleTrebuchet = LoadImage("Game/Assets/Images/trebuchet/trebuchet idle.png");
             Texture2D idleTexture = LoadTextureFromImage(idleTrebuchet);
             textures["Idle Trebuchet"] = idleTexture;
@@ -55,17 +52,21 @@ namespace Trebuchet.Game.Scripting
         }
         public void Execute(Ball ball, Castle castle, InputField counterWeight, InputField ballWeight, InputField counterHeight)
         {
+            //Begin drawing the screen
             BeginDrawing();
             ClearBackground(RAYWHITE);
 
             DrawBackground(textures["Background"]);
+            //If the ball exists, draw it
             if (ball.getExists())
                 DrawBall(textures["Ball"], ball);
 
+            //Draw the castle and input fields
             DrawCastle(textures["Castle"], castle);
             DrawInputField(counterWeight);
             DrawInputField(ballWeight);
             DrawInputField(counterHeight);
+            //If the ball exists and the input fields have been 
             if (!ball.getExists() && counterWeight.getCharCount() > 0 && ballWeight.getCharCount() > 0 && counterHeight.getCharCount() > 0)
             {
                 DrawSubmitButton(1150, 840, 185, 50);
@@ -75,7 +76,7 @@ namespace Trebuchet.Game.Scripting
                 checkCollisions.execute(ball, castle);
             }
             
-            
+            //Draw the equations
             DrawEquations(ball);
 
             //When the submit button is pressed, set the answers for each input field and reset launch
@@ -87,7 +88,7 @@ namespace Trebuchet.Game.Scripting
                 throwing = true;
                 launch = false;
             }
-
+            //Handle the trebuchet animation
             if (throwing == true)
             {
                 trebuchetTimer += GetFrameTime();
@@ -133,32 +134,24 @@ namespace Trebuchet.Game.Scripting
 
             EndDrawing();
         }
-        
-        bool IsAnyKeyPressed()
-        {
-            bool keyPressed = false;
-            int key = GetKeyPressed();
 
-            if ((key >= 32) && (key <= 126)) keyPressed = true;
-
-            return keyPressed;
-        }
+        //Draw the background
         private void DrawBackground(Texture2D texture)
         {
             DrawTexture(texture,  0, 0, WHITE);
         }
-
+        //Draw the ball
         private void DrawBall(Texture2D texture, Ball ball)
         {
             DrawTextureEx(texture, new Vector2((int)ball.getX(), (int)ball.getY()), 0, (float)ball.getScale(), WHITE);
 
         }
-
+        //Draw the castle
         private void DrawCastle(Texture2D texture, Castle castle)
         {
             DrawTextureEx(texture, new Vector2((int)castle.getPos().X, (int)castle.getPos().Y), 0, 1, WHITE);
         }
-
+        //Draw the equations
         private void DrawEquations(Ball ball)
         {
             //Positions
@@ -183,20 +176,19 @@ namespace Trebuchet.Game.Scripting
             DrawTextEx(Constants.font, $"Velocity = {String.Format("{0:0.00}", ball.getV())}m/s^2", velPos, 30, 1, BLACK);
             DrawTextEx(Constants.font, "Mass of CW =          kg", cwMassPos, 30, 1, BLACK);
         }
+
+        //Draw the input field
         private void DrawInputField(InputField inputField)
         {
-
+            //Check if the mouse clicks the input field
             if (CheckCollisionPointRec(GetMousePosition(), inputField.getRectangle()))
             {
                 if (IsMouseButtonDown(MouseButton.MOUSE_BUTTON_LEFT))
                     inputField.setTyping(true);
             }
-
+            //Check if the user is typing
             if (inputField.getTyping())
             {
-                // Set the window's cursor to the I-Beam
-                SetMouseCursor(MouseCursor.MOUSE_CURSOR_IBEAM);
-
                 // Get char pressed (unicode character) on the queue
                 int key = GetCharPressed();
 
@@ -260,55 +252,31 @@ namespace Trebuchet.Game.Scripting
                     }
                 }   
         }
+
+        //Draw the submit button
         private void DrawSubmitButton(int x, int y, int l, int w)
         {
             Rectangle button = new Rectangle(x, y, l, w);
             DrawRectangleRec(button, DARKGRAY);
             Vector2 submitPosition = new Vector2(x + 16, y + 5);
+            //Draw the text
             DrawTextEx(Constants.font, "LAUNCH", submitPosition, 40, 1, WHITE);
-
+            //Check if the mouse clicks the button
             if (CheckCollisionPointRec(GetMousePosition(), button))
             {
                 DrawRectangleRec(button, MAROON);
-                //DrawText("LAUNCH", (int)button.x + 8, (int)button.y + 8, 40, LIGHTGRAY);
                 DrawTextEx(Constants.font, "LAUNCH", submitPosition, 40, 1, WHITE);
                 if (IsMouseButtonDown(MouseButton.MOUSE_BUTTON_LEFT))
                     launch = true;
             }
         }
-
-        private void DrawPlayButton(int x, int y, int width, int height)
-        {
-            Rectangle button = new Rectangle(x, y, width, height);
-            DrawRectangleRec(button, DARKGRAY);
-            DrawText("Play Again", (int)button.x + 8, (int)button.y + 8, 40, WHITE);
-
-            if (CheckCollisionPointRec(GetMousePosition(), button))
-            {
-                DrawRectangleRec(button, MAROON);
-                DrawText("Play Again", (int)button.x + 8, (int)button.y + 8, 40, LIGHTGRAY);
-            }
-        }
-        private void DrawExitButton(int x, int y, int width, int height)
-        {
-            Rectangle button = new Rectangle(x, y, width, height);
-            DrawRectangleRec(button, DARKGRAY);
-            DrawText("Exit", (int)button.x + 8, (int)button.y + 8, 40, WHITE);
-
-            if (CheckCollisionPointRec(GetMousePosition(), button))
-            {
-                DrawRectangleRec(button, MAROON);
-                DrawText("Exit", (int)button.x + 8, (int)button.y + 8, 40, LIGHTGRAY);
-                if (IsMouseButtonDown(MouseButton.MOUSE_BUTTON_LEFT))
-                    // close game window
-                    CloseWindow();   
-            }
-        }
-
+    
+        //Draw the trebuchet texture
         private void DrawTrebuchet(Texture2D texture)
         {
             DrawTextureEx(texture, new Vector2(0, 400), 0, 1, WHITE);
         }
+        //Draw the trebuchet animation
         private void DrawTrebuchetAnimation(Texture2D texture, int frame)
         {
             float frameWidth = 400;
@@ -317,6 +285,7 @@ namespace Trebuchet.Game.Scripting
             Vector2 pos = new Vector2(0, 400);
             DrawTextureRec(texture, border, pos, WHITE);
         }
+        //Draw the explosion
         private void DrawExplosion(Texture2D texture, int frame)
         {
             float frameWidth = 300;
