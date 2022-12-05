1647b1ffa84a4a7d65688c678c97e0451ee34dba
Daniel Carr <92411898+DCPenguin73@users.noreply.github.com>
2022-12-05 12:02:30 -0700

    comments

--- Constants.cs
+++ Constants.cs
@@ -5,10 +5,10 @@ namespace Trebuchet
 {
     public class Constants
     {
-        public static double GRAVITY = 9.81;
-        public static double PROJECTILE_MASS = 1.5;
-        public static double COUNTERWEIGHT_HEIGHT = 10.1;
-        public static double PROJECTILE_HEIGHT = 5.25;
+        public static double GRAVITY = 9.81; // constant
+        public static double PROJECTILE_MASS = 1.5; //input
+        public static double COUNTERWEIGHT_HEIGHT = 10.1; //input
+        public static double PROJECTILE_HEIGHT = 5.25; //consnt
         public static double VELOCITY = 25.1;
 
         // public static double COUNTERWEIGHT_MASS = ((VELOCITY*VELOCITY)*PROJECTILE_MASS + 2*PROJECTILE_MASS*PROJECTILE_HEIGHT*GRAVITY)/(2*COUNTERWEIGHT_HEIGHT*GRAVITY);
